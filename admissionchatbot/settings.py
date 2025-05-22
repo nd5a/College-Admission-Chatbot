b1b7a920b918f6c7f5c1636c15da26423130fdd5
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2025-05-22 11:59:10 +0530

    Update settings.py

--- admissionchatbot/settings.py
+++ admissionchatbot/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = 'q!f3b!8jhlw7-_qft0(s7$bo6ctg$f$9hrv2^jqh276f&170+4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost',
+    '127.0.0.1',
+    'dn-collge-inquiry-bot.onrender.com',
+    '.onrender.com']
 
 
 # Application definition
